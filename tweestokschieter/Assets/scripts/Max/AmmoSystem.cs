38e3e6891ee5a50b76df5df7315b216e862d6fd0
max <maxwil2001@hotmail.com>
2019-05-06 15:47:16 +0200

    ammo system and gun

---
+++ tweestokschieter/Assets/scripts/Max/AmmoSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoSystem : MonoBehaviour
+{
+    public float ammo;
+    public float magSize = 125;
+    public GameObject bullet;
+
+    void Update()
+    {
+        ammo = Mathf.Clamp(ammo, 0, 25f);
+
+        ShootGun();
+
+        ReloadGun();
+
+    }
+
+    private void ReloadGun()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (magSize < magSize)
+            {
+                ReloadSystem();
+            }
+        }
+    }
+
+    private void ReloadSystem()
+    {
+        for (float i = ammo; i < magSize; i++)
+        {
+            if (magSize <= 0) break;
+            ammo++;
+            magSize--;
+        }
+    }
+
+    public void ShootGun()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            ammo--;
+        }
+    }
+}
---
+++ tweestokschieter/Assets/scripts/Max/Gun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject Bullet;
+
+ 
+    public void ShootGun()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(Bullet, transform.position, transform.rotation);
+        }
+    }
+}
