eaee694ed5af9e1948ae119864c58bbf44c8e4a3
Bouke <BoukeWeel@outlook.com>
2019-05-06 12:44:09 +0200

    dadfadf

---
+++ tweestokschieter/Assets/scripts/Bouke/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    Player player;
+    private void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        player.Armorpickup();
+    }
+}
