ce805741e13cc88b178edeb026add594682caa15
Bouke <BoukeWeel@outlook.com>
2019-05-06 12:03:31 +0200

    movement

---
+++ tweestokschieter/Assets/scripts/Bouke/lookat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookat : MonoBehaviour
+{
+    
+    void Update()
+    {
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit _hit;
+
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            transform.LookAt(_hit.point);
+
+        }
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+    }
+}
---
+++ tweestokschieter/Assets/scripts/Bouke/movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public KeyCode Rechts;
+    public KeyCode Down;
+    public KeyCode left;
+    public KeyCode up;
+    public int speed;
+    private void Update()
+    {
+        if (Input.GetKey(Rechts))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(left))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(up))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(Down))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+    }
+}
