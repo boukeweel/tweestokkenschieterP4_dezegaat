4ff715a811c190a56d8feb4ad6a33e9fe73ca401
max <maxwil2001@hotmail.com>
2019-05-07 10:28:37 +0200

    working door

---
+++ tweestokschieter/Assets/scripts/Max/OpenDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject Player;
+    public float range;
+    public LayerMask layerMask;
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Open();
+        }
+
+        
+    }
+
+    void Open()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Player.transform.position, Player.transform.forward, out hit, range, layerMask))
+        {
+            animator.SetBool("active", true);
+        }
+    }
+
+   
+}
