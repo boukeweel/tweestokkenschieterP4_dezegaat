e19968e88d5616b4e68dda1d117e8ad6036ea9a4
max <maxwil2001@hotmail.com>
2019-05-07 09:25:28 +0200

    more ammo

---
+++ tweestokschieter/Assets/scripts/Max/HealthSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int health;
+    public int Damage;
+
+    public void Health()
+    {
+        Damage = Damage - health;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
