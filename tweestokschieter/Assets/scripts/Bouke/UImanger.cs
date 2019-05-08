c197a58bf7fedd351e1667fc21b5fa2381e18247
Bouke <BoukeWeel@outlook.com>
2019-05-08 15:06:48 +0200

    UI

---
+++ tweestokschieter/Assets/scripts/Bouke/UImanger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UImanger : MonoBehaviour
+{
+    public GameObject mainmenu, credits, options, controls;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Credits()
+    {
+        mainmenu.SetActive(false);
+        options.SetActive(false);
+        controls.SetActive(false);
+        credits.SetActive(true);
+
+    }
+    public void Options()
+    {
+        mainmenu.SetActive(false);
+        options.SetActive(true);
+        controls.SetActive(false);
+        credits.SetActive(false);
+    }
+    public void MainMenu()
+    {
+        mainmenu.SetActive(true);
+        options.SetActive(false);
+        controls.SetActive(false);
+        credits.SetActive(false);
+    }
+   public void Controls()
+   {
+        mainmenu.SetActive(false);
+        options.SetActive(false);
+        controls.SetActive(true);
+        credits.SetActive(false);
+   }
+    public void GameQuit()
+    {
+        Application.Quit();  
+    }
+}
