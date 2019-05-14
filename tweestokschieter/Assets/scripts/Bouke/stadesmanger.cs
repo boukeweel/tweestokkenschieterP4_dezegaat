43e71e2415166a298a6751564148db0ca75242de
Bouke <BoukeWeel@outlook.com>
2019-05-14 09:56:25 +0200

    stades pie diagram

--- tweestokschieter/Assets/scripts/Bouke/stadesmanger.cs
+++ tweestokschieter/Assets/scripts/Bouke/stadesmanger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class stadesmanger : MonoBehaviour
 {
@@ -47,7 +48,27 @@ public class stadesmanger : MonoBehaviour
         RandomHealthpickedupammount = Random.Range(0, 200);
         RandomArmorpickedupAmmount = Random.Range(0, 200);
         acuraty = RandomShotHIt * 100 / Randomshotfired;
-
+        MakeCraph();
+        
+    }
+    private void MakeCraph()
+    {
+        float total = 0f;
+        float zRotation = 0f;
+        for (int i = 0; i < Values.Length; i++)
+        {
+            total += Values[i];
+            
+        }
+        for (int i = 0; i <Values.Length;i++)
+        {
+            Image Newwedge = Instantiate(WedgePrefabes) as Image;
+            Newwedge.transform.SetParent(transform, false);
+            Newwedge.color = WedgeColor[i];
+            Newwedge.fillAmount = Values[i] / total;
+            Newwedge.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, zRotation));
+            zRotation -= Newwedge.fillAmount * 360f;
+        }
     }
     private void Update()
     {
@@ -63,5 +84,9 @@ public class stadesmanger : MonoBehaviour
         Healthpickedupammount.text = RandomHealthpickedupammount.ToString();
         ArmorPickedupAmount.text = RandomArmorpickedupAmmount.ToString();
 
+    }
+    public void tomainmenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
