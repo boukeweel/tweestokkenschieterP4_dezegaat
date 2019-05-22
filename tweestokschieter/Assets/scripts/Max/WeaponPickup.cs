490ae8cb63d2928ab7c90f66edb42393f063af55
max <maxwil2001@hotmail.com>
2019-05-22 11:48:25 +0200

    backup van de backup van de backup van de tutorial level

---
+++ tweestokschieter/Assets/scripts/Max/WeaponPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour
+{
+    [SerializeField] private Transform[] weapons;
+    [SerializeField] private Transform player;
+    private bool gunInHand;
+
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.collider.CompareTag("");
+    }
+}
