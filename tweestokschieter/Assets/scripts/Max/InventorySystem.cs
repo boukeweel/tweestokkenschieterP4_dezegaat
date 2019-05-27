dbb24da2f40bac586205214f706ffe681f9dc4db
max <maxwil2001@hotmail.com>
2019-05-27 12:00:39 +0200

    workstation

--- tweestokschieter/Assets/scripts/Max/InventorySystem.cs
+++ tweestokschieter/Assets/scripts/Max/InventorySystem.cs
@@ -17,19 +17,58 @@ public class InventorySystem : MonoBehaviour
 
     public Animator animator;
 
-    private void OnTriggerEnter(Collider other)
+    public bool inventoryActive = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("desk"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (inventoryActive)
+                {
+                    PlayeAnimaiton();
+                    if (Item1 > 1 && Item2 > 2 && Item3 > 3)
+                    {
+
+                    }
+                }
+                else
+                {
+                    ReverseAnimtion();
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
     {
         if(other.CompareTag("desk"))
         {
-            if(Item1 > 1 && Item2 > 2 && Item3 > 3)
-            {
-                MakeHealthPotion();
-            }
+            PlayeAnimaiton();
         }
     }
 
     public void MakeHealthPotion()
     {
         healthPotion++;
+    }
+
+    public void PlayeAnimaiton()
+    {
+        inventoryActive = false;
+        Time.timeScale = 1f;
+        animator.SetBool("active", false);
+
+    }
+
+    public void ReverseAnimtion()
+    {
+        animator.SetBool("active", true);
+        inventoryActive = true;
+    }
+
+    public void SetTime()
+    {
+        Time.timeScale = 0f;
     }
 }
