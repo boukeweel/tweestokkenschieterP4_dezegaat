ea48970bcdb30371874f2fa5bbb6d1a227e44d15
Bouke <BoukeWeel@outlook.com>
2019-06-17 11:08:06 +0200

    Auto stash before merge of "master" and "origin/master"

--- tweestokschieter/Assets/scripts/Bouke/upgrades.cs
+++ tweestokschieter/Assets/scripts/Bouke/upgrades.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class upgrades : MonoBehaviour
 {
     public AmmoSystem hetweapon;
+    
 
     public void Upgradeclipsize()
     {
@@ -13,17 +14,52 @@ public class upgrades : MonoBehaviour
     public void shotgunnow()
     {
         
+        if(hetweapon.Switchtoshotgun == true)
+        {
+            Debug.Log("already shotgun");
+        }
+        else
+        {
+            hetweapon.UpgradeToShutgun();
+        }
+            
     }
     public void autofirenow()
     {
-
+        if(hetweapon.Switchtoautofire == true)
+        {
+            Debug.Log("alraedy autofire");
+        }
+        else
+        {
+            hetweapon.UpgradeToAutofire();
+        }
     }
     public void simifirenow()
     {
-
+        if(hetweapon.Switchtoautofire == false && hetweapon.Switchtoshotgun == false)
+        {
+            Debug.Log("already simifire");
+        }
+        else
+        {
+            hetweapon.UpradeToSimifire();
+        }
     }
     public void lessreloadtimer()
     {
-
+        if(hetweapon.reloadTime < 0.5)
+        {
+            return;
+        }
+        hetweapon.upgradeRelaodtimer();
+    }
+    public void upgradeshootingspeed()
+    {
+        if(hetweapon.timetowait < 0.3)
+        {
+            return;
+        }
+        hetweapon.Upgradetimetowait();
     }
 }
